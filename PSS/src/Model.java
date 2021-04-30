6acd43fba6e09b8b37bc468402bd815d8eb7a11a
Ean Nagahashi <eanandrew@outlook.com>
2021-04-30 14:31:37 -0700

    Set up the template for most of the classes

---
+++ PSS/src/Model.java
@@ -0,0 +1,18 @@
+public class Model {
+    Task[] listOfTasks;
+    
+    public void createTask(String name, String type, float startTime, float duration){
+    }
+
+    public void roundTime(){
+    }
+
+    public void viewTask(){
+    }
+
+    public void deleteTask(){
+    }
+
+    public void editTask(){
+    }
+}
---
+++ PSS/src/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    String name;
+    String type;
+    float startTime;
+    float duration;
+
+    public Task(String name, String type, float startTime, float duration){
+    }
+
+    public void checkStartTime(){
+    }
+
+    public void checkDurationTime(){
+    }
+
+    public void roundTime(){
+    }
+
+    public void checkTaskType(){
+    }
+
+    public void checkTaskName(){
+    }
+
+    public void checkOverlaps(){
+    }
+}
