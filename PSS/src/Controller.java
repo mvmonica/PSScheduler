e63f2a98d17dd33a55db007b96b9dfb659ce572d
Ean Nagahashi <eanandrew@outlook.com>
2021-05-04 12:28:58 -0700

    Testing Task creation

--- PSS/src/Controller.java
+++ PSS/src/Controller.java
@@ -1,6 +1,10 @@
 public class Controller {
     public static void main(String[] args) throws Exception{
-        
+        Task testTask = new Task("Class", "Recurring", 12.26f, 4.5f);
+        System.out.println(testTask.name);
+        System.out.println(testTask.type);
+        System.out.println(testTask.startTime);
+        System.out.println(testTask.duration);
     }
 
     public void writeSchedule(String fileName){
