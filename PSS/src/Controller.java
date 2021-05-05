0d5c828ad6c0ec5e1d05983952ea7895b9d03e45
Arsham1024 <Arsham512@gmail.com>
2021-05-04 20:28:53 -0700

    Create Task in Model is now accepting the inputs and putting them in JSON file format. in main find some example uses. Also a directory named JASON_Files need to be created to store the created JSON files.

--- PSS/src/Controller.java
+++ PSS/src/Controller.java
@@ -1,15 +1,10 @@
 public class Controller {
     public static void main(String[] args) throws Exception{
-        Model testModel = new Model();
-        testModel.createTask("CS 3560", "Class", 12.26f, 4.5f); //basic task test
+        Model m = new Model();
 
-        testModel.createTask("CS 1400", "Class", 12.26f, 4.5f, 1234567, 1234567, 7); //recurring task test
-
+        m.createTask("Sleep", "Transient" , 0200f, 0300f);
 
 
-        testModel.viewTask("CS 3560");
-        System.out.println();
-        testModel.viewTask("CS 1400");
     }
 
     public void writeSchedule(String fileName){
