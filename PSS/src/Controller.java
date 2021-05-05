e183dfd96059407c65361ba895aaaa9a1e3a34d0
Ean Nagahashi <eanandrew@outlook.com>
2021-05-04 18:20:48 -0700

    testing test cases for recurring task

--- PSS/src/Controller.java
+++ PSS/src/Controller.java
@@ -1,8 +1,15 @@
 public class Controller {
     public static void main(String[] args) throws Exception{
         Model testModel = new Model();
-        testModel.createTask("CS 3560", "Class", 12.26f, 4.5f);
+        testModel.createTask("CS 3560", "Class", 12.26f, 4.5f); //basic task test
+
+        testModel.createTask("CS 1400", "Class", 12.26f, 4.5f, 1234567, 1234567, 7); //recurring task test
+
+
+
         testModel.viewTask("CS 3560");
+        System.out.println();
+        testModel.viewTask("CS 1400");
     }
 
     public void writeSchedule(String fileName){
