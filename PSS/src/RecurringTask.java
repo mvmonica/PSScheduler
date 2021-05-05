18b1e3cb56abf018877e488c936eb1a8a4ae7e50
Ean Nagahashi <eanandrew@outlook.com>
2021-05-04 18:21:53 -0700

    Fixed the constructor

--- PSS/src/RecurringTask.java
+++ PSS/src/RecurringTask.java
@@ -3,8 +3,11 @@ public class RecurringTask extends Task{
     int endDate;
     int frequency;
 
-    public RecurringTask(String name, String type, float startTime, float duration, float endDate, int frequency){
+    public RecurringTask(String name, String type, float startTime, float duration, int startDate, int endDate, int frequency){
         super(name, type, startTime, duration);
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.frequency = frequency;
     }
 
     public void compareDates(int startDate, int endDate){
