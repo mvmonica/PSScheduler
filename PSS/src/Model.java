12cffb8d37c95bc9ddfd018f5abfbc61aee57a1f
Ean Nagahashi <eanandrew@outlook.com>
2021-05-04 18:21:23 -0700

    added arrayLists

--- PSS/src/Model.java
+++ PSS/src/Model.java
@@ -1,25 +1,24 @@
 import java.util.ArrayList;
 
 public class Model {
-
-    
-    Task[] listOfTasks; //going to change array to arrayList make edits to the schedule easier
-
-    int listSize = 0;
     ArrayList<Object> testList = new ArrayList<Object>();
     
     public void createTask(String name, String type, float startTime, float duration){
         testList.add(new Task(name, type, startTime, duration));
+      
     }
 
-    public void roundTime(){
-
+    public void createTask(String name, String type, float startTime, float duration, int startDate, int endDate, int frequency){
+        testList.add(new RecurringTask(name, type, startTime, duration, startDate, endDate, frequency));
     }
+
+    
 
     public void viewTask(String name){
         for (int i = 0; i < testList.size(); i++){
             Task temp = (Task)testList.get(i);
             if (temp.name.equals(name)){
+                //displays a task, but should be replaced later
                 System.out.println("Name: " + temp.name);
                 System.out.println("Type: " + temp.type);
                 System.out.println("Start Time: " + temp.startTime);
@@ -27,20 +26,18 @@ public class Model {
                 break;
             }
         }
-
-
- 
     }
 
     public void deleteTask(String name){
-        for (Task taskThing: listOfTasks){
-            if(taskThing.name.equals(name)){
-                
+        for (int i = 0; i < testList.size(); i++){
+            Task temp = (Task)testList.get(i);
+            if (temp.name.equals(name)){
+                testList.remove(i);
             }
         }
     }
 
-    public void editTask(){
+    public void editTask(String name){
     }
 
 }
