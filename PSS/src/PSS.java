b43d15b8157fa03700108394bd40f0d79f1e1238
Maddie <mejiamadeline23@gmail.com>
2021-05-18 13:57:10 -0700

    added fields to the pages for user input

--- PSS/src/PSS.java
+++ PSS/src/PSS.java
@@ -1,5 +1,3 @@
-//package pss;
-
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.application.Application;
@@ -40,10 +38,13 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.web.WebEngine;
+import java.time.LocalDate;
+import javafx.scene.control.DatePicker;
 
 
 public class PSS extends Application
 {
+    private static final Scene scene = null;
     // to hold the overall page and be able to change scenes
     private BorderPane mainLayout = new BorderPane();
     private static Stage guiStage;
@@ -156,7 +157,7 @@ public class PSS extends Application
         
         
         //Vbox for the create button's page
-        Label temp = new Label("Code goes here: ");
+        Label temp = new Label("");
         VBox createPageVbox = new VBox(temp);
         createPageVbox.setPadding(new Insets(15));
         createPageVbox.setAlignment(Pos.CENTER);
@@ -166,10 +167,40 @@ public class PSS extends Application
         createPage.setHgap(15);
         createPage.setVgap(15);
         createPage.setPadding(new Insets(15));
-        createPage.setAlignment(Pos.CENTER);
+        createPage.setAlignment(Pos.TOP_LEFT);
+        final ComboBox taskComboBox = new ComboBox();
+        taskComboBox.getItems().addAll("Transient Task", "Recurring Task", "AntiTask");
+        final ComboBox typetaskComboBox = new ComboBox();
+        typetaskComboBox.getItems().addAll("Class", "Study", "Sleep", "Exercise", "Work", "Meal", "Visit", "Shopping", "Appointment", "Cancellation");
+        final ComboBox frequencyComboBox = new ComboBox();
+        frequencyComboBox.getItems().addAll("Daily", "Weekly");
+        final TextField name = new TextField();
+        final TextField duration = new TextField();
+        final TextField startTime = new TextField();
+        DatePicker startDatePicker = new DatePicker();
+        DatePicker endDatePicker = new DatePicker();
+        startDatePicker.setValue(LocalDate.now());
+        endDatePicker.setValue(startDatePicker.getValue().plusDays(1));
+        createPage.add(new Label("Name of task: "), 0, 0);
+        createPage.add(name, 1, 0);
+        createPage.add(new Label("Select the style of task:"), 0, 1);
+        createPage.add(taskComboBox, 1, 1);
+        createPage.add(new Label("Select the type of task:"), 0, 2);
+        createPage.add(typetaskComboBox, 1, 2);
+        createPage.add(new Label("Select the start date:"), 0, 3);
+        createPage.add(startDatePicker, 1, 3);
+        createPage.add(new Label("Select the end date:"), 0, 4);
+        createPage.add(endDatePicker, 1, 4);
+        createPage.add(new Label("Start time of the task: "), 0, 5);
+        createPage.add(startTime, 1, 5);
+        createPage.add(new Label("Duartion of the task: "), 0, 6);
+        createPage.add(duration, 1, 6);
+        createPage.add(new Label("Frequency: "), 0, 7);
+        createPage.add(frequencyComboBox, 1, 7);
+        
         
         //Vbox for the view button's page
-        Label temp2 = new Label("And here: ");
+        Label temp2 = new Label("");
         VBox viewPageVbox = new VBox(temp2);
         viewPageVbox.setPadding(new Insets(15));
         viewPageVbox.setAlignment(Pos.CENTER);
@@ -179,10 +210,13 @@ public class PSS extends Application
         viewPage.setHgap(15);
         viewPage.setVgap(15);
         viewPage.setPadding(new Insets(15));
-        viewPage.setAlignment(Pos.CENTER);
+        viewPage.setAlignment(Pos.TOP_LEFT);
+        final TextField name2 = new TextField();
+        viewPage.add(new Label("Name of task: "), 0, 0);
+        viewPage.add(name2, 1, 0);
         
         //Vbox for the delete button's page
-        Label temp3 = new Label("Code goes here: ");
+        Label temp3 = new Label("");
         VBox deletePageVbox = new VBox(temp3);
         deletePageVbox.setPadding(new Insets(15));
         deletePageVbox.setAlignment(Pos.CENTER);
@@ -192,10 +226,13 @@ public class PSS extends Application
         deletePage.setHgap(15);
         deletePage.setVgap(15);
         deletePage.setPadding(new Insets(15));
-        deletePage.setAlignment(Pos.CENTER);
+        deletePage.setAlignment(Pos.TOP_LEFT);
+        final TextField name3 = new TextField();
+        deletePage.add(new Label("Name of task: "), 0, 0);
+        deletePage.add(name3, 1, 0);
         
         //Vbox for the edit button's page
-        Label temp4 = new Label("here: ");
+        Label temp4 = new Label("");
         VBox editPageVbox = new VBox(temp4);
         editPageVbox.setPadding(new Insets(15));
         editPageVbox.setAlignment(Pos.CENTER);
@@ -205,7 +242,37 @@ public class PSS extends Application
         editPage.setHgap(15);
         editPage.setVgap(15);
         editPage.setPadding(new Insets(15));
-        editPage.setAlignment(Pos.CENTER);
+        editPage.setAlignment(Pos.TOP_LEFT);
+        final ComboBox newtaskComboBox = new ComboBox();
+        newtaskComboBox.getItems().addAll("Transient Task", "Recurring Task", "AntiTask");
+        final ComboBox newtypetaskComboBox = new ComboBox();
+        newtypetaskComboBox.getItems().addAll("Class", "Study", "Sleep", "Exercise", "Work", "Meal", "Visit", "Shopping", "Appointment", "Cancellation");
+        final ComboBox newfrequencyComboBox = new ComboBox();
+        newfrequencyComboBox.getItems().addAll("Daily", "Weekly");
+        final TextField name4 = new TextField();
+        final TextField newduration = new TextField();
+        final TextField newstartTime = new TextField();
+        DatePicker newstartDatePicker = new DatePicker();
+        DatePicker newendDatePicker = new DatePicker();
+        newstartDatePicker.setValue(LocalDate.now());
+        newendDatePicker.setValue(newstartDatePicker.getValue().plusDays(1));
+        editPage.add(new Label("Name of task: "), 0, 0);
+        editPage.add(name4, 1, 0);
+        editPage.add(new Label("Select new style of task:"), 0, 1);
+        editPage.add(newtaskComboBox, 1, 1);
+        editPage.add(new Label("Select the new type of task:"), 0, 2);
+        editPage.add(newtypetaskComboBox, 1, 2);
+        editPage.add(new Label("Select the new start date:"), 0, 3);
+        editPage.add(newstartDatePicker, 1, 3);
+        editPage.add(new Label("Select the new end date:"), 0, 4);
+        editPage.add(newendDatePicker, 1, 4);
+        editPage.add(new Label("New start time of the task: "), 0, 5);
+        editPage.add(newstartTime, 1, 5);
+        editPage.add(new Label("New duartion of the task: "), 0, 6);
+        editPage.add(newduration, 1, 6);
+        editPage.add(new Label("New frequency: "), 0, 7);
+        editPage.add(newfrequencyComboBox, 1, 7);
+
         
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //if any of the text on the 1st page is clicked add the specific layout container to the scene
